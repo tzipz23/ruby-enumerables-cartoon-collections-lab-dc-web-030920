19e6f9eb44f252143b9e58ba47ec09ce6bbd5df7
TOMAZYE ANDERSON <tomazyeanderson@yahoo.com>
2020-03-03 20:37:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,9 @@ def roll_call_dwarves(array)# code an argument here
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
+
+
   # Your code here
 end
 

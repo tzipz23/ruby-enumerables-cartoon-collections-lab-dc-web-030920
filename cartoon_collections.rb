71a4ea9847f2c88f6236bdcebd234ef4e17ad6bd
TOMAZYE ANDERSON <tomazyeanderson@yahoo.com>
2020-03-03 20:37:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,9 @@ def summon_captain_planet(array)# code an argument here
   # Your code here
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
+
+
   # Your code here
 end
 
